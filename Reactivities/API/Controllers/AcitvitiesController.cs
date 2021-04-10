1a87d1abcdd1107abf5cc96605b5155b9117d201
Allan Wu <allan.wu6@gmail.com>
2021-04-10 22:07:45 +1000

    Added Details Handler

--- Reactivities/API/Controllers/AcitvitiesController.cs
+++ Reactivities/API/Controllers/AcitvitiesController.cs
@@ -17,12 +17,10 @@ namespace Reactivities.API.Controllers
             return await Mediator.Send(new List.Query());
         }
 
-        /*
         [HttpGet("{id}")]
-        public Task<ActionResult<Activity>> GetActivity(Guid id)
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return Ok();
+            return await Mediator.Send(new Details.Query{Id = id});
         }
-        */
     }
 }
---
+++ Reactivities/Application/Activities/Details.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Reactivities.Domain;
+using Reactivities.Persistence;
+
+namespace Reactivities.Application.Activities
+{
+    public class Details
+    {
+        public class Query : IRequest<Activity>
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Activity>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+
+            }
+            public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _context.Activities.FindAsync(request.Id);
+            }
+        }
+    }
+}
