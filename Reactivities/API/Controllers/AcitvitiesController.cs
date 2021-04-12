6556bcabd82465287e7e171c4420c8b6bb2b1ef6
Allan Wu <allan.wu6@gmail.com>
2021-04-12 19:56:31 +1000

    Added Delete Activity Handler

--- Reactivities/API/Controllers/AcitvitiesController.cs
+++ Reactivities/API/Controllers/AcitvitiesController.cs
@@ -33,5 +33,10 @@ namespace Reactivities.API.Controllers
             activity.id = id;
             return Ok(await Mediator.Send(new Edit.Command {Activity = activity}));
         }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id) {
+            return Ok(await Mediator.Send(new Delete.Command{Id = id}));
+        }
     }
 }
