6c1285e688c23cd14d072c2c2a82dc7c308e109c
Allan Wu <allan.wu6@gmail.com>
2021-04-26 22:09:31 +1000

    Implementing deleting an activity using MobX

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,23 +1,18 @@
+import { observer } from 'mobx-react-lite';
 import React, { SyntheticEvent, useState } from 'react';
 import { Button, Item, Label, Segment } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity';
 import { useStore } from '../../../app/stores/store';
 
-interface Props {
-    activities: Activity[];
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-}
+export default observer(function ActivityList() {
+    const {activityStore} = useStore();
+    const {deleteActivity, activities, loading} = activityStore;
 
-export default function ActivityList({activities, deleteActivity, submitting}: Props) {
     const [target, setTarget] = useState('');
 
     function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
         setTarget(e.currentTarget.name);
         deleteActivity(id);
     }
-
-    const {activityStore} = useStore();
 
     return (
         <Segment>
@@ -33,7 +28,7 @@ export default function ActivityList({activities, deleteActivity, submitting}: P
                             </Item.Description>
                             <Item.Extra>
                                 <Button onClick={() => activityStore.selectActivity(activity.id)} floated='right' content='View' color='blue'/>
-                                <Button name={activity.id} loading={submitting && target === activity.id} onClick={(e) => handleActivityDelete(e, activity.id)} floated='right' content='Delete' color='red'/>
+                                <Button name={activity.id} loading={loading && target === activity.id} onClick={(e) => handleActivityDelete(e, activity.id)} floated='right' content='Delete' color='red'/>
                                 <Label basic content={activity.category}/>
                             </Item.Extra>
                         </Item.Content>
@@ -42,4 +37,4 @@ export default function ActivityList({activities, deleteActivity, submitting}: P
             </Item.Group>
         </Segment>
     )
-}
+})
