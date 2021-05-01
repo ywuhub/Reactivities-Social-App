11a8063a10d4a2ad94180f7f363e9da338c67bc8
Allan Wu <allan.wu6@gmail.com>
2021-05-01 23:11:44 +1000

    Refactoring Code: Cleaning up unused code

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -5,6 +5,7 @@ import { Button, Form, Segment } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
 import { v4 as uuid } from 'uuid';
+import { Link } from 'react-router-dom';
 
 export default observer(function ActivityForm() {
     
@@ -60,7 +61,7 @@ export default observer(function ActivityForm() {
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange}/>
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange}/>
                 <Button loading={loading} floated='right' positive type='submit' content='Submit'/>
-                <Button floated='right' type='button' content='Cancel'/>
+                <Button as={Link} to='/activities' floated='right' type='button' content='Cancel'/>
             </Form>
         </Segment>
     )
