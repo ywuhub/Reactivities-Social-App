01e20ec143d933fd9287599a22944983281609d7
Allan Wu <allan.wu6@gmail.com>
2021-05-27 22:51:38 +1000

    Small fix for wrong classname so style wasn't used

--- client-app/src/features/home/homepage.tsx
+++ client-app/src/features/home/homepage.tsx
@@ -4,7 +4,7 @@ import { Container, Header, Segment, Image, Button } from 'semantic-ui-react';
 
 export default function HomePage() {
       return (
-            <Segment inverted textAlign='center' className='mastHead'>
+            <Segment inverted textAlign='center' vertical className='masthead'>
                   <Container text>
                         <Header as='h1' inverted>
                               <Image size='massive' src='/assets/logo.png' style={{marginBottom: 12}} />
