5cf5fb632771493bb0665fd43f0481c709475c59
Allan Wu <allan.wu6@gmail.com>
2021-05-31 21:00:46 +1000

    Adding User DTOs (Data Transfer Objects)
    DTOs or Data Transfer Objects is an object that carries data between processes

---
+++ Reactivities/API/DTOs/RegisterDTO.cs
@@ -0,0 +1,10 @@
+namespace API.DTOs
+{
+    public class RegisterDTO
+    {
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
