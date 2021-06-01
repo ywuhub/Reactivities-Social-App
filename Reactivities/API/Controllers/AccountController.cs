250a64f4af6bc149fa91e20a8a7c18e571948d84
Allan Wu <allan.wu6@gmail.com>
2021-06-01 21:10:10 +1000

    Implemented Token Service for User Authentication

--- Reactivities/API/Controllers/AccountController.cs
+++ Reactivities/API/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using API.DTOs;
+using API.Services;
 using Domain;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -12,8 +13,10 @@ namespace API.Controllers
       {
             private readonly UserManager<AppUser> _userManager;
             private readonly SignInManager<AppUser> _signInManager;
-            public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+            private readonly TokenService _tokenService;
+            public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, TokenService tokenService)
             {
+                  _tokenService = tokenService;
                   _signInManager = signInManager;
                   _userManager = userManager;
             }
@@ -21,24 +24,24 @@ namespace API.Controllers
             [HttpPost("login")]
             public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
             {
-                var user = await _userManager.FindByEmailAsync(loginDTO.Email);
+                  var user = await _userManager.FindByEmailAsync(loginDTO.Email);
 
-                if (user == null) return Unauthorized();
+                  if (user == null) return Unauthorized();
 
-                var result = await _signInManager.CheckPasswordSignInAsync(user, loginDTO.Password, false);
+                  var result = await _signInManager.CheckPasswordSignInAsync(user, loginDTO.Password, false);
 
-                if (result.Succeeded)
-                {
-                    return new UserDTO
-                    {
-                        DisplayName = user.DisplayName,
-                        Image = null,
-                        Token = "This will be a generated token",
-                        Username = user.UserName
-                    };
-                }
+                  if (result.Succeeded)
+                  {
+                        return new UserDTO
+                        {
+                              DisplayName = user.DisplayName,
+                              Image = null,
+                              Token = _tokenService.CreateToken(user),
+                              Username = user.UserName
+                        };
+                  }
 
-                return Unauthorized();
+                  return Unauthorized();
             }
       }
 }
