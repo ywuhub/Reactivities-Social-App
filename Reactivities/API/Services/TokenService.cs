3189bb941a9f4d3695377afd49f20807fac33192
Allan Wu <allan.wu6@gmail.com>
2021-06-02 22:12:05 +1000

    Implemented storing secrets during Dev Environment

--- Reactivities/API/Services/TokenService.cs
+++ Reactivities/API/Services/TokenService.cs
@@ -5,35 +5,42 @@ using System.Text;
 using Domain;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Extensions.Configuration;
 
 namespace API.Services
 {
-    public class TokenService
-    {
-        public string CreateToken(AppUser user) 
-        {
-            var claims = new List<Claim>
+      public class TokenService
+      {
+            private readonly IConfiguration _configuration;
+            public TokenService(IConfiguration configuration)
+            {
+                  _configuration = configuration;
+
+            }
+            public string CreateToken(AppUser user)
+            {
+                  var claims = new List<Claim>
             {
                 new Claim(ClaimTypes.Name, user.UserName),
                 new Claim(ClaimTypes.NameIdentifier, user.Id),
                 new Claim(ClaimTypes.Email, user.Email)
             };
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super secret key"));
-            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+                  var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["TokenKey"]));
+                  var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.Now.AddDays(7),
-                SigningCredentials = credentials
-            };
+                  var tokenDescriptor = new SecurityTokenDescriptor
+                  {
+                        Subject = new ClaimsIdentity(claims),
+                        Expires = DateTime.Now.AddDays(7),
+                        SigningCredentials = credentials
+                  };
 
-            var tokenHandler = new JwtSecurityTokenHandler();
-            
-            var token = tokenHandler.CreateToken(tokenDescriptor);
+                  var tokenHandler = new JwtSecurityTokenHandler();
 
-            return tokenHandler.WriteToken(token);
-        }
-    }
+                  var token = tokenHandler.CreateToken(tokenDescriptor);
+
+                  return tokenHandler.WriteToken(token);
+            }
+      }
 }
