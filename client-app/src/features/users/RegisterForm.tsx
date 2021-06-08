768f279e2f2fb237a9aa81a7bcce40facd808dcb
Allan Wu <allan.wu6@gmail.com>
2021-06-08 20:03:12 +1000

    Implemented Registration Form

--- client-app/src/features/users/RegisterForm.tsx
+++ client-app/src/features/users/RegisterForm.tsx
@@ -4,23 +4,32 @@ import React from 'react';
 import { Button, Header, Label } from 'semantic-ui-react';
 import MyTextInput from '../../app/common/form/MyTextInput';
 import { useStore } from '../../app/stores/store';
+import * as Yup from 'yup';
 
-export default observer(function LoginForm() {
+export default observer(function RegisterForm() {
       const {userStore} = useStore();
       return (
             <Formik
-                  initialValues={{email: '', password: '', error: null}}
+                  initialValues={{displayName: '', username: '', email: '', password: '', error: null}}
                   onSubmit={(values, {setErrors}) => userStore.login(values).catch(error => setErrors({error: 'Invalid Email or Password Entered'}))}
+                  validationSchema={Yup.object({
+                        displayName: Yup.string().required(),
+                        username: Yup.string().required(),
+                        email: Yup.string().required().email(),
+                        password: Yup.string().required(),
+                  })}
             >
-                  {({handleSubmit, isSubmitting, errors}) => (
+                  {({handleSubmit, isSubmitting, errors, isValid, dirty}) => (
                         <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-                              <Header as='h2' content='Login to Reactivities' color='teal' textAlign='center' />
+                              <Header as='h2' content='Sign up to Reactivities' color='teal' textAlign='center' />
+                              <MyTextInput name='displayName' placeholder='Display Name' />
+                              <MyTextInput name='username' placeholder='Username' />
                               <MyTextInput name='email' placeholder='Email' />
                               <MyTextInput name='password' placeholder='Password' type='password' />
                               <ErrorMessage 
                                     name='error' render={() => <Label style={{maginBottom: 10}} basic color='red' content={errors.error} />}
                               />
-                              <Button loading positive content='Login' type='submit' fluid />
+                              <Button disabled={!isValid || !dirty || isSubmitting} loading positive content='Register' type='submit' fluid />
                         </Form>
                   )}
             </Formik>
