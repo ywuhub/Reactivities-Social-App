d8be7707c2425235de9de162c289afc12ad1fad9
Allan Wu <allan.wu6@gmail.com>
2021-06-26 21:51:15 +1000

    Updated mapping configuration

--- Reactivities/Application/Core/MappingProfiles.cs
+++ Reactivities/Application/Core/MappingProfiles.cs
@@ -18,6 +18,8 @@ namespace Reactivities.Application.Core
                 .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
                 .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
                 .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio));
+            CreateMap<AppUser, Profiles.Profile>()
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url));
         }
     }
 }
