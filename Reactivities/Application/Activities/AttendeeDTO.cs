f3c8a16d374df0b7a2269b72a3df903d9231b4a7
Allan Wu <allan.wu6@gmail.com>
2021-06-27 22:56:46 +1000

    Implementing Attendee DTO

---
+++ Reactivities/Application/Activities/AttendeeDTO.cs
@@ -0,0 +1,10 @@
+namespace Application.Activities
+{
+    public class AttendeeDTO
+    {
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+        public string Bio { get; set; }
+        public string Image { get; set; }
+    }
+}
