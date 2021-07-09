ee40c943aa383d032065f21df1604cf6e89a03fc
Allan Wu <allan.wu6@gmail.com>
2021-07-09 22:42:24 +1000

    Added Comment Data Transfer Object

--- Reactivities/Application/Core/MappingProfiles.cs
+++ Reactivities/Application/Core/MappingProfiles.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using Application.Activities;
+using Application.Comments;
 using AutoMapper;
 using Domain;
 using Reactivities.Domain;
@@ -21,6 +22,10 @@ namespace Reactivities.Application.Core
                 .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
             CreateMap<AppUser, Profiles.Profile>()
                 .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url));
+            CreateMap<Comment, CommentDTO>()
+                .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.Author.DisplayName))
+                .ForMember(d => d.Username, o => o.MapFrom(s => s.Author.UserName))
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.Author.Photos.FirstOrDefault(x => x.IsMain).Url));
         }
     }
 }
--- Reactivities/Domain/Comment.cs
+++ Reactivities/Domain/Comment.cs
@@ -7,7 +7,7 @@ namespace Domain
     {
         public int Id { get; set; }
         public string Body { get; set; }
-        public AppUser Auther { get; set; }
+        public AppUser Author { get; set; }
         public Activity Activity { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
