4b302124e12bc4a9d157d820d07c66649338d5af
Allan Wu <allan.wu6@gmail.com>
2021-07-11 20:09:32 +1000

    Added Comment List Handler

---
+++ Reactivities/Application/Comments/List.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Reactivities.Application.Core;
+using Reactivities.Persistence;
+
+namespace Application.Comments
+{
+      public class List
+      {
+            public class Query : IRequest<Result<List<CommentDTO>>>
+            {
+                  public Guid ActivityId { get; set; }
+            }
+
+            public class Handler : IRequestHandler<Query, Result<List<CommentDTO>>>
+            {
+                  private readonly DataContext _context;
+                  private readonly IMapper _mapper;
+                  public Handler(DataContext context, IMapper mapper)
+                  {
+                        _mapper = mapper;
+                        _context = context;
+                  }
+
+                  public async Task<Result<List<CommentDTO>>> Handle(Query request, CancellationToken cancellationToken)
+                  {
+                        var comments = await _context.Comments
+                            .Where(x => x.Activity.id == request.ActivityId)
+                            .OrderBy(x => x.CreatedAt)
+                            .ProjectTo<CommentDTO>(_mapper.ConfigurationProvider)
+                            .ToListAsync();
+
+                        return Result<List<CommentDTO>>.Success(comments);
+                  }
+            }
+      }
+}
