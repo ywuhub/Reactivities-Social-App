362d0e9b34e4681dad401cca91e16ce9009ea8fe
Allan Wu <allan.wu6@gmail.com>
2021-07-19 21:47:00 +1000

    Added methods to follow/unfollow

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -94,7 +94,9 @@ const Profiles = {
         })
     },
     setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
-    deletePhoto: (id: string) => requests.del(`/photos/${id}`, {})
+    deletePhoto: (id: string) => requests.del(`/photos/${id}`, {}),
+    updateProfile: (profile: Partial<Profile>) => requests.put('/profiles', profile),
+    updateFollowing: (username: string) => requests.post(`/follow/{username}`, {})
 }
 
 const agent = {
