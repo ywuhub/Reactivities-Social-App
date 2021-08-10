6eef25676ce60b2a45d383da7067f3a658406d21
Allan Wu <allan.wu6@gmail.com>
2021-08-10 20:46:36 +1000

    Heroku Deploy

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using Persistence;
+using System;
 
 namespace API.Extensions
 {
